c9bb6f03327434bcb550889f2756263b26d93e98
mohamed dmt <mohamedeldamaty111@gmail.com>
2026-05-01 23:13:00 +0300

    change key state to status

--- app/components/Buildingslot.tsx
+++ app/components/Buildingslot.tsx
@@ -15,16 +15,16 @@ export function buildSlots(data: ApiResponse): Islot[] {
 
   return times.map((time, index) => {
     const reservation = reservations.find((res) =>
-      isSlotBooked(time, [res], slotDuration)
+      isSlotBooked(time, [res], slotDuration),
     );
 
     let state: Eslotstate = Eslotstate.available;
 
     if (reservation) {
-      if (reservation.state.toLowerCase() === "maintained") {
-        state = Eslotstate.maintained;
-      } else if (reservation.state.toLowerCase() === "booked") {
+      if (reservation.status.toLowerCase() === "confirmed") {
         state = Eslotstate.booked;
+      } else {
+        state = Eslotstate.maintained;
       }
     }
 
